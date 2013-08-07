dbe42f6285136cc3b8122f1628094264e4adbdf1
Suki <suki@missallsunday.com>
2013-08-07 12:02:35 -0500

    Creating the extra column and covering my butt with a proper license header
    Signed-off-by: Suki <suki@missallsunday.com>

--- install.php
+++ install.php
@@ -1,1 +1,34 @@
 <?php
+
+/**
+ *
+ * @package SAIOM mod
+ * @version 1.0
+ * @author Jessica González <suki@missallsunday.com>
+ * @copyright Copyright (c) 2013, Jessica González
+ * @license http://www.mozilla.org/MPL/2.0/
+ */
+
+	if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('SMF'))
+		require_once(dirname(__FILE__) . '/SSI.php');
+
+	elseif (!defined('SMF'))
+		exit('<b>Error:</b> Cannot install - please verify you put this in the same place as SMF\'s index.php.');
+
+	global $smcFunc, $context;
+
+	db_extend('packages');
+
+	if (empty($context['uninstalling']))
+		$smcFunc['db_add_column'](
+			'{db_prefix}messages',
+			array(
+				'name' => 'show_thumbnail',
+				'type' => 'int',
+				'size' => 1,
+				'null' => false
+			),
+			array(),
+			'update',
+			null
+		);
